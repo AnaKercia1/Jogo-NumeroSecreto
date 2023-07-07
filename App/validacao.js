c515d4eba78bfe26db044ada95fe3a0db49015ca
Ana Kércia <123599474+AnaKercia1@users.noreply.github.com>
2023-07-07 15:07:22 -0400

    Atualizando arquivos

--- App/validacao.js
+++ App/validacao.js
@@ -1,6 +1,16 @@
 function verificaSeOChutePossuiUmValorValido(chute) {
   const numero = +chute;
 
+  if (chute.toUpperCase() === "GAME OVER") {
+    document.body.innerHTML = `
+    <h2> GAME OVER </h2>
+    <h3> O número secreto era ${numeroSecreto} </h3>
+
+    <button id="jogar-novamente" class="btn-jogar"> Jogar novamente </button> 
+    `;
+    document.body.style.background = "#404258";
+  }
+
   if (chuteForInvalido(numero)) {
     elementoChute.innerHTML += "<div>Valor inválido</div>";
     return;
@@ -16,7 +26,20 @@ function verificaSeOChutePossuiUmValorValido(chute) {
         <button id="jogar-novamente" class="btn-jogar">
         Jogar Novamente
         </button>
-        `
+        `;
+    const startit = () => {
+      setTimeout(function () {
+        confetti.start();
+      }, 100);
+    };
+
+    const stopit = () => {
+      setTimeout(function () {
+        confetti.stop();
+      }, 5000);
+    };
+    startit();
+    stopit();
   } else if (numero > numeroSecreto) {
     elementoChute.innerHTML += ` 
     <div>O número secreto é menor <i class="fa-solid fa-circle-arrow-down"></i></div-->
